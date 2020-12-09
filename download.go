e7fa9469e7f28bebb7c1c30fa3554be65d838c59
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2020-12-09 15:35:17 +0100

    try return the body

--- download.go
+++ download.go
@@ -1,7 +1,6 @@
 package aoc
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"io"
@@ -10,7 +9,7 @@ import (
 )
 
 // DownloadInput downloads the requested input file.
-func (c *Client) DownloadInput(year, day int) (io.Reader, error) {
+func (c *Client) DownloadInput(year, day int) (io.ReadCloser, error) {
 	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
 
 	req, err := http.NewRequestWithContext(context.Background(), "GET", url, nil)
@@ -20,20 +19,16 @@ func (c *Client) DownloadInput(year, day int) (io.Reader, error) {
 
 	req.AddCookie(&http.Cookie{Name: "session", Value: c.SessionToken})
 
-	resp, err := c.HTTPClient.Do(req)
+	resp, err := c.HTTPClient.Do(req) //nolint:bodyclose
 	if err != nil {
 		return nil, err
 	}
-
-	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		return nil, DownloadError{StatusCode: resp.StatusCode}
 	}
 
-	content := bufio.NewReader(resp.Body)
-
-	return content, nil
+	return resp.Body, nil
 }
 
 // DownloadAndSaveInput downloads and saves the requested input file.
@@ -42,6 +37,8 @@ func (c *Client) DownloadAndSaveInput(year, day int, targetFile string) error {
 	if err != nil {
 		return err
 	}
+
+	defer file.Close()
 
 	f, err := os.Create(targetFile)
 	if err != nil {
